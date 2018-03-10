ffd182bc8975ebe954b5da131c21b79932fc7ba2
Edgar David Garay Forero <edgarayf@unal.edu.co>
2018-03-10 02:58:07 +0000

    primeros componentes y rutas

---
+++ picaditosf/src/Components/Register.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+
+// The Roster component matches one of two different routes
+// depending on the full pathname
+const Register = () => (
+  <div>
+    <h1>Welcome to registro!</h1>
+  </div>
+)
+
+
+export default Register
