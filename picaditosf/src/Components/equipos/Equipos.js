3462390077325319663416a07fa76461c9f21cf5
David Perdomo <daeperdomocr@unal.edu.co>
2018-05-17 04:59:44 +0000

    improve teams view

--- picaditosf/src/Components/equipos/Equipos.js
+++ picaditosf/src/Components/equipos/Equipos.js
@@ -33,69 +33,38 @@ export default class Equipos extends Component {
     return (<div>
 
         {Example}
-
         </div>); // render the loading component
     }
 
-
     return (
-
-<div>
-
-<div className="cont_2">
-		<div className="container">
-
-		  	<p  style={divStyle}>En esta pagina puede ver los equipos registrados si quiere crear o buscar un equipo puede hacelor llenando el siguiente formulario</p>
-        <div class="row">
-        <div class="col-sm-2" align="center"></div>
-        <div class="col-sm-2" align="center">
-        <a href='/createE'>
-            <button className="btn">Crear equipo</button>
-        </a>
-        </div>
-        <div class="col-sm-4" align="center"></div>
-        <div class="col-sm-2" align="center">
+    <div>
+    <div className="cont_2">
+      <h1>Equipos</h1>
+      <div className="scrollable row">
+          <div className="text-center cont_card col-md-3">
+            <div className="">
+              <a href='/createE'>
+                <img src='https://cdn0.iconfinder.com/data/icons/flat-social-media-icons-set-round-style-1/550/netvibes-512.png' className="img-responsive profile-img"/>
+            	  <h4>Crear Equipo</h4>
+            	</a>
+            	</div>
+          </div>  
+          { this.state.eventos.map(equipo =>
+          <div className="text-center cont_card col-md-3">
+            <Link to={`/equipo/${equipo.id}`}> 
+              <img src={require('../../imagenes/team.jpg')} className="img-responsive profile-img"/>
+              <h4>{equipo.nombre}</h4>
+            </Link>
+          </div>
+          )}
+      </div>
+      <div className="" align="center">
         <a href='/BuscarEquipo'>
-            <button className="btn">Buscar equipo</button>
+          <button className="btn">Buscar equipo</button>
         </a>
-        </div>
-        </div>
-
-		</div>
-</div>
-
-{ this.state.eventos.map(evento =>
-	 <div className="cont_2">
-		<div className="container">
-		<div className="row align-items-start">
-		  	<div className="col-md-2">
-		  		<img src={require('../../imagenes/team.jpg')} className="img-responsive profile-img"/>
-		  	</div>
-
-		  	<div className="col-md-8">
-		  		<h1>
-		  		 {evento.nombre}
-		  	    </h1>
-
-		  		<h4>Capitan: {evento.capitan_name}</h4>
-		  	</div>
-
-		  	<div className="col-md-2 " >
-
-              <Link to={`/equipo/${evento.id}`}>
-		  		  		<button className="btn btn-info prf-btn">Ver Equipo</button>
-
-							</Link>
-		  	</div>
-		</div>
-		</div>
-		</div>
-		)}
-
-	</div>
-
-
-
+  		</div>
+    </div>
+    </div>
     )
   }
 }
