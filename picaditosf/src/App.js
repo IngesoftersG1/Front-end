ee1eba089822727d3b068051e917eb88cca4461d
David Perdomo <daeperdomocr@unal.edu.co>
2018-05-29 06:27:36 +0000

    back btn dummy

--- picaditosf/src/App.js
+++ picaditosf/src/App.js
@@ -7,6 +7,9 @@ const App = () => (
   <div>
 		<Main />
 	 	<Header />
+	 	<span className="backimg" onClick={()=>window.history.back()} >
+      <img src="https://cdn4.iconfinder.com/data/icons/Ethereal_Icons_2/PNGs/Toolbar/Browser%20Toolbar/Back.png" width="42"/>
+    </span>
     <footer className="footer">
       <div className="container">
       	<div className="text-center">
--- picaditosf/src/consts.js
+++ picaditosf/src/consts.js
@@ -1,3 +1,3 @@
-
-export const SERVER_URL = "http://localhost:3001/"
+export const SERVER_URL = "https://back-end-proyect-daeperdomocr.c9users.io/"
+//export const SERVER_URL = "http://localhost:3001/"
 
